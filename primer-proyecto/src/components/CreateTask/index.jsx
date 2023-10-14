7ce96ad095912faa3cc4c777048ad47daed89c1e
linder3hs <linderhassinger00@gmail.com>
2023-10-14 08:33:46 -0500

    ➕ feat: add select

--- primer-proyecto/src/components/CreateTask/index.jsx
+++ primer-proyecto/src/components/CreateTask/index.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import { Modal } from "../../components";
+import { Listbox } from "@headlessui/react";
+import { categories } from "../../core/category";
 
 export default function CreateTask() {
   const [open, setOpen] = useState(false);
 
+  const [selectCategory, setSelectCategory] = useState(categories[0]);
+
   return (
     <>
       <Modal open={open} onClose={() => setOpen(!open)}>
-        <h1>Es un modal</h1>
+        <form action="">
+          <h2>Crear Tarea</h2>
+          <div className="my-5">
+            <div>
+              <input
+                type="text"
+                placeholder="Escribe tu tarea"
+                className="w-full border px-2 py-3 rounded outline-none"
+              />
+            </div>
+            <div>
+              <Listbox value={selectCategory} onChange={setSelectCategory}>
+                <Listbox.Button>{selectCategory.text}</Listbox.Button>
+                <Listbox.Options>
+                  {categories.map((category) => (
+                    <Listbox.Option key={category.text} value={category.text}>
+                      <img src={category.icon} /> {category.text}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Listbox>
+            </div>
+          </div>
+        </form>
       </Modal>
       <button
         onClick={() => setOpen(!open)}
