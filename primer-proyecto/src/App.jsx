a9849669d0694ce10f3f75e0e6d532faf8d0175a
linder3hs <linderhassinger00@gmail.com>
2023-10-15 09:48:34 -0500

    ➕ feat: add router

--- primer-proyecto/src/App.jsx
+++ primer-proyecto/src/App.jsx
@@ -1,52 +1,5 @@
-import { useState, useEffect } from "react";
-import { CreateTask, TodoList, TodoListCategory } from "./components";
-import { categories } from "./core/category";
-import { read } from "./services";
+import Router from "./router";
 
 export default function App() {
-  const [currentCategory, setCurrentCategory] = useState(null);
-
-  const [tasks, setTasks] = useState([]);
-
-  const getTasks = async () => {
-    const data = await read(); // {ok: bool, response: []}
-    setTasks(data.response);
-  };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl">Manage your time well</h1>
-      <div className="my-6">
-        <h2 className="font-semibold">Categories {currentCategory}</h2>
-        <div className="flex mt-3 justify-between">
-          {categories.map((category) => (
-            <TodoListCategory
-              key={category.text}
-              icon={category.icon}
-              text={category.text}
-              setCurrentCategory={setCurrentCategory}
-              currentCategory={currentCategory}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <h2 className="font-semibold">
-          You have {tasks.length} task for today
-        </h2>
-        <button
-          onClick={() => setCurrentCategory(null)}
-          className="text-sm text-blue-500"
-        >
-          Clear filter
-        </button>
-      </div>
-      <TodoList tasks={tasks} category={currentCategory} />
-      <CreateTask getTasks={getTasks} />
-    </div>
-  );
+  return <Router />;
 }
