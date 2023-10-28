92333dc9ab043b1f829f29b17f0f13af46cc99d4
linder3hs <linderhassinger00@gmail.com>
2023-10-28 11:14:48 -0500

    ➕ feat: add nextflix-clone

---
+++ netflix-clone/src/common/components/TextField/index.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent } from "react";
+
+interface Props {
+  name?: string;
+  type?: string;
+  placeholder?: string;
+  id?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+}
+
+export default function TextField(props: Props) {
+  return (
+    <>
+      <input
+        {...props}
+        className="w-full py-4 px-3 text-sm rounded bg-[#333] border-none outline-none placeholder-white"
+      />
+    </>
+  );
+}
+
+TextField.defaultProps = {
+  type: "text",
+};
---
+++ netflix-clone/src/login/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { TextField } from "@/common";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="bg-[url(/images/bg-login.jpg)] h-screen">
+        <div className="fixed inset-0 bg-black/60" />
+        <div className="relative">
+          <Image
+            src="/images/logo.png"
+            alt="Logo de Netflix"
+            width={200}
+            height={200}
+          />
+          <div className="text-white w-1/4 mx-auto bg-black/60 mt-5 rounded px-10 py-5">
+            <h2 className="text-2xl">Iniciar Sesión</h2>
+            <form action="" className="flex flex-col mt-5 gap-5">
+              <TextField type="email" placeholder="Email" />
+              <TextField type="password" placeholder="Password" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
